f6aa206f44fe8f8fdd4a8dee591c25a61872f25a
Daksh-21 <dakshvasu21@gmail.com>
2024-07-20 00:01:04 +0530

    datatypesandvaribles

---
+++ datatypesandvaribles.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    cout<<a<<endl;
+
+    char ch = 'D';
+    cout<<ch<<endl;
+
+    float pie = 3.14;
+    cout<<pie<<endl;
+
+    double d = 2.10001;
+    cout<<d<<endl;
+
+    bool flag = true;
+    cout<<flag<<endl;
+}
