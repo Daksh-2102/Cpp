223878dfe81ae10f0caafd3a1246fbbabda71487
Daksh-21 <dakshvasu21@gmail.com>
2024-07-25 14:42:16 +0530

    greater

---
+++ greater.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter a :";
+    cin>>a;
+
+    int b;
+    cout<<"Enter b :";
+    cin>>b;
+
+    if(a>b){
+        cout<<a<<" is Greater";
+    }
+    else{
+        cout<<b<<" is Greater";
+    }
+    return 0;
+}
