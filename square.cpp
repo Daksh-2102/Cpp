d7a9e1910bfa1029294a2fef1106dd18105aa2e2
Daksh-21 <dakshvasu21@gmail.com>
2024-07-25 16:51:32 +0530

    SquareuptoN

---
+++ square.cpp
@@ -0,0 +1,12 @@
+//Square upto n numbers..
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    
+    for(int i=1;i<=n;i++){
+        cout<<i*i<<"\n";
+    }
+}
