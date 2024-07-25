5c7e317d4c477d3cb925bd6f08acfa4a1fcc1475
Daksh-21 <dakshvasu21@gmail.com>
2024-07-25 18:01:04 +0530

    ApPattern

---
+++ ap.cpp
@@ -0,0 +1,15 @@
+// 1 4 7 10 .... upto n
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n;i=i+3){
+        cout<<i<<" ";
+    }
+
+
+   return 0;
+}
