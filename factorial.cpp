20523cac7af8eaf7e3a186bed4a91b9edbf22e2d
Daksh-21 <dakshvasu21@gmail.com>
2024-07-25 18:57:40 +0530

    Factorial

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+//Factorial of that number eg : 5! = 120
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int fact = 1;
+    for(int i=1;i<=n;i++){
+        fact = fact*i;
+    }
+    cout<<fact;
+}
