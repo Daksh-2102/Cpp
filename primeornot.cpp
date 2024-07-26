154aa93578b59bef19dbe7b9e12643bc901862a5
Daksh-21 <dakshvasu21@gmail.com>
2024-07-26 13:47:21 +0530

    Primeno

---
+++ primeornot.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+   if(n==1) {
+        cout<<n<<" is Neither prime nor composite";
+    return 0;
+   }
+   if(n<=0){
+        cout<<n<<" is Not prime";
+        return 0;
+   }
+   else{
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            cout<<n<<" is composite";
+            return 0;
+        }
+    }
+       cout<<n<<" is prime";
+
+    }
+
+
+
+}
