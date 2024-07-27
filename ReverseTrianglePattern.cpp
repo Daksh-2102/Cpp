18de9f3741d7af2b181f843374c91ff96b669fa0
Daksh-21 <dakshvasu21@gmail.com>
2024-07-27 14:31:05 +0530

    ReverseNumTriangle

--- ReverseTrianglePattern.cpp
+++ ReverseTrianglePattern.cpp
@@ -1,7 +1,7 @@
 // 5
 // 5 4
 // 5 4 3
-// 5 4 3 2 
+// 5 4 3 2
 // 5 4 3 2 1
 #include<iostream>
 using namespace std;
@@ -9,9 +9,9 @@ int main(){
     int n;
     cin>>n;
 
-    for(int i=1;i<=n;i++){
-        for(int j=n;j>=n-i+1;j--){
-            cout<<j<<" ";
+    for(int i=n;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            cout<<i<<" ";
         }
         cout<<endl;
     }
