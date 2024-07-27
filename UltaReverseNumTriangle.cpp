f7f63b09c0bcb3a55939713970f60920b50319a5
Daksh-21 <dakshvasu21@gmail.com>
2024-07-27 14:19:49 +0530

    UltaReverseNumberTriangle

---
+++ UltaReverseNumTriangle.cpp
@@ -0,0 +1,18 @@
+// 5
+// 5 4
+// 5 4 3
+// 5 4 3 2 
+// 5 4 3 2 1
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=n;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
