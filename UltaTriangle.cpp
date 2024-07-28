d9108b12dfc0e81d1ca745cc488f79539b5e09f1
Daksh-21 <dakshvasu21@gmail.com>
2024-07-28 19:16:15 +0530

    UltaTriangle

--- UltaTriangle.cpp
+++ UltaTriangle.cpp
@@ -1,8 +1,4 @@
-//         * 
-//       * * * 
-//     * * * * * 
-//   * * * * * * * 
-// * * * * * * * * *  
+  
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,7 +6,7 @@ int main(){
     cin>>n;
 
     // Rows
-    for(int i=1;i<=n;i++){
+    for(int i=n;i>=1;i--){
         for(int j=1;j<=n-i;j++){
             cout<<"  ";
         }
