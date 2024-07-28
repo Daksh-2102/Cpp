bb9510c8b4c6cb7c266613fe605ba2e86809bfb7
Daksh-21 <dakshvasu21@gmail.com>
2024-07-28 17:41:40 +0530

    OddTriangle

--- OddTriangle.cpp
+++ OddTriangle.cpp
@@ -1,14 +1,15 @@
-//     * 
-//    * * 
-//   * * * 
-//  * * * * 
-// * * * * * 
+//        * 
+//       * * * 
+//     * * * * * 
+//   * * * * * * * 
+// * * * * * * * * *  
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
 
+    // Rows
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n-i;j++){
             cout<<"  ";
