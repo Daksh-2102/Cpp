d31ca24827b0ee583d06350cdacb9ad82a4128d1
Daksh-21 <dakshvasu21@gmail.com>
2024-07-29 22:50:18 +0530

    Day3HomeworkQ1

---
+++ HOMEWORK/Day3HwQ1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"a : ";
+    cin>>a;
+
+    int b;
+    cout<<"b : ";
+    cin>>b;
+
+    int product = a*b;
+    cout<<product;
+    return 0;
+}
