eed343dab5cb599b45b7e0b652ba2e509ed5b4ee
Daksh-21 <dakshvasu21@gmail.com>
2024-07-29 22:53:25 +0530

    Day3HomeworkQ2

---
+++ HOMEWORK/Day3hwQ2.cpp
@@ -0,0 +1,16 @@
+//Subtraction
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"a : ";
+    cin>>a;
+
+    int b;
+    cout<<"b : ";
+    cin>>b;
+
+    int sub = a-b;
+    cout<<"ans = "<<sub;
+    return 0;
+}
