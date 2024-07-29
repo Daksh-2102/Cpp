47c7bc6bd141b232ed83c4bc0935547983d54301
Daksh-21 <dakshvasu21@gmail.com>
2024-07-29 23:02:32 +0530

    Day4HomeworkQ1

---
+++ HOMEWORK/Day4HwQ1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"a : ";
+    cin>>a;
+
+    int b;
+    cout<<"b : ";
+    cin>>b;
+
+    if(a>b){
+        cout<<a<<" is Biggest";
+    }
+    else{
+        cout<<b<<" is Biggest";
+    }
+    return 0;
+}
