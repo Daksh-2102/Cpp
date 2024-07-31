3be07eeaad7b6bec5f75d53be327ab1e6ed3cab3
Daksh-21 <dakshvasu21@gmail.com>
2024-07-31 22:04:18 +0530

    AssignmentOperator

---
+++ AssinmentOperator.cpp
@@ -0,0 +1,24 @@
+// Assignmentt Operator
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    a*=3;
+    cout<<a<<endl;
+    int b = 10;
+    b/=10;
+    cout<<b<<endl;
+    int c = 10;
+    c+=10;
+    cout<<c<<endl;
+    int d = 10;
+    d-=10;
+    cout<<d<<endl;
+    int e = 8;
+    e%=10;
+    cout<<e<<endl;
+
+
+
+
+}
