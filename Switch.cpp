efbbfd995d7f9689c88b7f86895e0f9fd76eef45
Daksh-21 <dakshvasu21@gmail.com>
2024-08-01 18:37:12 +0530

    Switch

---
+++ Switch.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cin>>i;
+    
+    switch(i){
+       case 1:{
+         cout<<"Daksh";
+         break;
+       }
+       case 2:{
+         cout<<"Vasu";
+         break;
+       }
+       case 3:{
+         cout<<"Raghu";
+         break;
+       }
+       case 4:{
+         cout<<"jass";
+         break;
+       }
+       default:{
+         cout<<"Nothing";
+       }
+
+    }
+}
