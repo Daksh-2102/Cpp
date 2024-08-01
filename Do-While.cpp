939ddc38563bccc3e085f51c6e4c40d33b674e0e
Daksh-21 <dakshvasu21@gmail.com>
2024-08-01 17:41:02 +0530

    Do-While

---
+++ Do-While.cpp
@@ -0,0 +1,14 @@
+// Do-while
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    
+    int i=1;// initialize
+    do{
+        cout<<i<<endl;
+        i++; // updation
+    } while(i<=n); // break
+}
