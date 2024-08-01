f683826412485163dda94800691d774ee9b449f9
Daksh-21 <dakshvasu21@gmail.com>
2024-08-01 17:55:48 +0530

    EvenNoUsingWhileLoop

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 int main(){
     int n;
+    cin>>n;
+
+    int i=1;
+    cout<<"Even numbers are: "<<endl;
+    while(i<=n){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+        i++;
+    }
+}
