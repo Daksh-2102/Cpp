aa36bbdebb1b68895e4c33aff5f0b08a7418cb47
Daksh-21 <dakshvasu21@gmail.com>
2024-08-02 15:00:08 +0530

    Day12HomeworkQ5

---
+++ HOMEWORK/Day12HwQ5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 2*3-48==5/4*6;
+    cout<<a<<endl;
+    int b = 6<<2-4*8/2;
+    cout<<b<<endl;
+    int c = 5>4<3/2-8%4+5;
+    cout<<c<<endl;
+    int d = 14-8+92>>2+70;
+    cout<<d<<endl;
+
+    return 0;
+}
