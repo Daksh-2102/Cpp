8c73c104e167a17460ec7fe43dd9b6addee7a671
Daksh-21 <dakshvasu21@gmail.com>
2024-08-04 13:14:16 +0530

    Leetcode:6_Palindrome

---
+++ LeetCode/6_Palindrome.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    int num;
+    num = n;
+
+    int reverse = 0;
+
+    while(n>0){
+        int lastdigit = n % 10;
+        n/=10;
+        reverse = reverse * 10 + lastdigit;
+    }
+    // num = reverse;
+
+    if(reverse==num){
+        cout<<num<<" is palindrome";
+    }
+    else{
+        cout<<num<<" is Not palindrome";
+    }
+    
+}
