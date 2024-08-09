bb3b6181aa292c483dbb08dd69baf838541e9e9a
Daksh-21 <dakshvasu21@gmail.com>
2024-08-09 15:41:31 +0530

    MissingNumber

---
+++ Arrays/MissingElement.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin>>n;
+
+
+    int arr[n];
+    cout<<"Enter the elements of an array : ";
+    for(int i=0;i<n-1;i++){
+        cin>>arr[i];
+    }
+
+    int sum = 0;
+    for(int i=0;i<n-1;i++){
+        sum+=arr[i];
+    }
+
+    int s2 = (n * (n+1) ) / 2;
+
+    cout<<"Missing number = "<<s2-sum;
+
+
+
+
+    
+
+}
