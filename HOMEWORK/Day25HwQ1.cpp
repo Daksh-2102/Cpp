8958ca1f8bb401874d5ed83dd042f41079774c93
Daksh-21 <dakshvasu21@gmail.com>
2024-08-12 21:51:33 +0530

    Day25HomeworkQ1

---
+++ HOMEWORK/Day25HwQ1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void display(int n,int arr[]){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    } cout<<endl;
+}
+
+void Selection_Sort(int n,int arr[]){
+    for(int i=0;i<n-1;i++){
+        int index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]>arr[index])
+            {
+                index=j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin>>n;
+
+    int arr[1000];
+    cout<<"Enter the elements of the array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Array Before Sorting : ";
+    display(n,arr);
+
+    Selection_Sort(n,arr);
+    cout<<"Array After Sorting : ";
+    display(n,arr);
+}
