8fbcb792bcad78d9b4c9d7fc6b7d9a6820cf4ddb
Daksh-21 <dakshvasu21@gmail.com>
2024-08-13 07:51:31 +0530

    Day29HomeworkQ1

---
+++ HOMEWORK/Day29HwQ1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void display(int n,int arr[]){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    } cout<<endl;
+}
+
+void Insertion_sort(int n,int arr[]){
+    for(int i=1;i<n;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j]>arr[j-1])
+            swap(arr[j],arr[j-1]);
+            else
+            break;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin>>n;
+
+    int arr[1000];
+    cout<<"Enter the elements of the array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Array Before Sorting : ";
+    display(n,arr);
+
+    Insertion_sort(n,arr);
+    cout<<"Array After Sorting : ";
+    display(n,arr);
+}
