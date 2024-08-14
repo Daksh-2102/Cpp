3b69d508692d1e9c06f48b5205698d506a871a81
Daksh-21 <dakshvasu21@gmail.com>
2024-08-14 12:51:57 +0530

    Leetcode:17_CubeRoot

---
+++ LeetCode/17_CubeRoot.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int Sqrt(int k){
+    int start = 0 , end = k,ans;
+
+    while(start<=end){
+        int mid = start + (end-start)/2;
+
+        if(mid*mid==k){
+            ans = mid;
+            break;
+        }
+
+        else if(mid*mid<k){
+            ans=mid;
+            start=mid+1;
+        }
+
+        else 
+        end=mid-1;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number  : ";
+    cin>>n;
+
+    cout<<"Square Root of "<< n <<" = "<<Sqrt(n);
+
+}
