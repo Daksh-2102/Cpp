649dcdecfe51592fd44d3ef24123f79a73475eb8
Daksh-21 <dakshvasu21@gmail.com>
2024-08-14 12:39:53 +0530

    Leetcode:5_Sqrt

--- LeetCode/5_Sqrt.cpp
+++ LeetCode/5_Sqrt.cpp
@@ -1,9 +1,33 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
+
+int Sqrt(int k){
+    int start = 0 , end = k,ans;
+
+    while(start<=end){
+        int mid = start + (end-start)/2;
+
+        if(mid*mid==k){
+            ans = mid;
+            break;
+        }
+
+        else if(mid*mid<k){
+            ans=mid;
+            start=mid+1;
+        }
+
+        else 
+        end=mid-1;
+    }
+    return ans;
+}
+
 int main(){
     int n;
+    cout<<"Enter the number  : ";
     cin>>n;
 
-    int a = sqrt(n);
-    cout<<a;
+    cout<<"Square Root of "<< n <<" = "<<Sqrt(n);
+
 }
