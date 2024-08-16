4384e094d58e07a5cb6e911dcdec9be813ae0bb1
Daksh-21 <dakshvasu21@gmail.com>
2024-08-16 13:38:18 +0530

    Searching_Index_in_Rotated_array

---
+++ SortingAndSearchingMethods/Search_in_Rotated_array.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+int Search_Element_in_Rotated_Array(int n,int arr[],int target){
+    int start = 0, end = n-1,mid;
+
+    while(start<=end){
+        mid = start + (end-start)/2;
+
+        if(arr[mid]==target)
+        return mid;
+
+        else if(arr[mid]>=arr[0]){// this means left side is sorted 
+            // this if checks that target is in left side or not
+            if(arr[start]<=target && target<=arr[mid]){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+
+        else { // this else shows (arr[mid]<=arr[0]) // right side sort hai
+            //   
+
+            if(arr[mid]<=target && target<=arr[end]){
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+
+        }
+    }
+    return -1;
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter the size of an array : ";
+    cin>>n;
+
+    int arr[1000];
+    cout<<"Enter the elements of an array :";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int target;
+    cout<<"Enter the element to search in rotated in array : ";
+    cin>>target;
+
+    cout<<"Index of taregt in Rotated array = "<<Search_Element_in_Rotated_Array(n,arr,target);
+    
+}
