016f26c77c3907ebe1edd668117baa003e0351ab
Daksh-21 <dakshvasu21@gmail.com>
2024-08-29 06:38:25 +0530

    Linear_Search

--- Arrays/Searching_Index.cpp
+++ Arrays/Searching_Index.cpp
@@ -1,29 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int SearchingIndex(int n,int arr[],int k){
+    int index = -1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==k){
+            index = i;
+        }
+    }
+    return index;
+}
+
 int main(){
     int n;
     cin>>n;
-    
+
     int arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
 
-    int element ;
-    cout<<"Element to search : ";
-    cin>>element;
+    int k;
+    cout<<"Enter the element to search : ";
+    cin>>k;
 
-    int index = -1;
-    for(int i=0;i<n;i++){
-        if(arr[i]==element){
-            index = i;
-            break;
-        }
-        cout<<element<<" found at index = "<<index;
-    }
-    //return -1;
-
-   
-    
+    //SearchingIndex
+    cout<<k<<" found at index = "<<SearchingIndex(n,arr,k);
 }
